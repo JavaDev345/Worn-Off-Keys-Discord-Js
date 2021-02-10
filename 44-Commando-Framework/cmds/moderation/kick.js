5a0c586252a2181d71ccaf70931366f0f5f9bbea
JavaDev345 <75867289+JavaDev345@users.noreply.github.com>
2021-02-10 10:25:45 -0500

    Update kick.js

--- 44-Commando-Framework/cmds/moderation/kick.js
+++ 44-Commando-Framework/cmds/moderation/kick.js
@@ -15,7 +15,7 @@ module.exports = class KickCommand extends Commando.Command {
   async run(message) {
     const target = message.mentions.users.first()
     if (!target) {
-      message.reply('Please specify someone to kick')
+      message.channel.send('Please specify someone to kick')
       return
     }
 
@@ -24,9 +24,9 @@ module.exports = class KickCommand extends Commando.Command {
     const member = guild.members.cache.get(target.id)
     if (member.kickable) {
       member.kick()
-      message.reply('That user has been kicked')
+      message.channel.send('That user has been kicked')
     } else {
-      message.reply('I cannot kick that user')
+      message.channel.send('I cannot kick that user')
     }
   }
 }
